21410ef2f998a2241a7654ea618280caf8345d5f
Damion Gibbs <dmgibbs@writeme.com>
2018-08-06 17:32:33 +0000

    Step 3 of the project activity completed

---
+++ step3.js
@@ -0,0 +1,45 @@
+
+// while https is built-in to Node, it is a module, so it must be required
+
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+var https = require('https');
+
+function getAndPrintHTML (options) {
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
+
+
+  var buffer =[];
+
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+      buffer += data.toString();  // keep storing the data chunk that was received
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(buffer); // display the data received.
+    });
+
+  });
+
+}
+
+getAndPrintHTML(requestOptions);
